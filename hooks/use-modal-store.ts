04ddbc46cb60163840676b9036e5c843114415b2
Akylbek04 <zamirovakylbek920@gmail.com>
2023-10-19 00:31:25 +0200

    modal for managing members added

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,7 @@
 import { Channel, ChannelType, Server } from "@prisma/client";
 import { create } from "zustand";
 
-export type ModalType = "createServer" | "invite" | "editServer";
+export type ModalType = "createServer" | "invite" | "editServer" | "members";
 
 interface ModalData {
   server?: Server;
