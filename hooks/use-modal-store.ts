033b630ff9e2fda1ab5e74f38ae7f425493330b9
Akylbek04 <zamirovakylbek920@gmail.com>
2023-10-18 11:27:07 +0200

    added edit_server

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,25 +1,10 @@
 import { Channel, ChannelType, Server } from "@prisma/client";
 import { create } from "zustand";
 
-export type ModalType =
-  | "createServer"
-  | "invite"
-  | "editServer"
-  | "members"
-  | "createChannel"
-  | "leaveServer"
-  | "deleteServer"
-  | "deleteChannel"
-  | "editChannel"
-  | "messageFile"
-  | "deleteMessage";
+export type ModalType = "createServer" | "invite" | "editServer";
 
 interface ModalData {
   server?: Server;
-  channel?: Channel;
-  channelType?: ChannelType;
-  apiUrl?: string;
-  query?: Record<string, any>;
 }
 
 interface ModalStore {
